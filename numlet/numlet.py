958c79e8136a027e2967178b4905e84ab2c358a9
Roylan Martinez <58228934+roylanmartinez@users.noreply.github.com>
2020-01-22 03:06:30 +0100

    Update numlet.py

--- numlet/numlet.py
+++ numlet/numlet.py
@@ -2,35 +2,42 @@ class Numesp:
     def __init__(self, x):
         self.x = str(x)
         self.largo = len(self.x)
+
     def __str__(self):
-        return self.printer()
+        if 1 <= self.largo <= 3:
+            doo = 'self.tres(self.x)'
+        if 4 <= self.largo <= 6:
+            doo = 'self.seis(self.x)'
+        return eval(doo)
+
     # UNIDADES
-    def unidades(self, x, un=False,trae=''):
-        if x == '0':
-            return 'Cero'
-        elif x == '1':
+    def unidades(self, x, un=False, trae=''):
+        if x == '1' or x == '01' or x == '001':
             if un == False:
                 return 'Uno'
-            return 'Un'
-        elif x == '2':
+            else:
+                return 'Un'
+        elif x == '2' or x == '02' or x == '002':
             return 'Dos'
-        elif x == '3':
+        elif x == '3' or x == '03' or x == '003':
             return 'Tres'
-        elif x == '4':
+        elif x == '4' or x == '04' or x == '004':
             return 'Cuatro'
-        elif x == '5':
+        elif x == '5' or x == '05' or x == '005':
             return 'Cinco'
-        elif x == '6':
+        elif x == '6' or x == '06' or x == '006':
             return 'Seis'
-        elif x == '7':
+        elif x == '7' or x == '07' or x == '007':
             return 'Siete'
-        elif x == '8':
+        elif x == '8' or x == '08' or x == '008':
             return 'Ocho'
-        else:
+        elif x == '9' or x == '09' or x == '009':
             return 'Nueve'
+        else:
+            return 'error'
 
     # DECENAS
-    def decenas(self, x, un=False,trae=''):
+    def decenas(self, x, un=False, trae=''):
         if x == '10':
             return 'Diez'
         elif x == '11':
@@ -83,16 +90,65 @@ class Numesp:
             return 'Setenta'
         elif x == '80':
             return 'Ochenta'
-        else:
+        elif x == '90':
             return 'Noventa'
-    # IMPRESOR
-    def printer(self,trae=False):
-        if len(self.x)==1:
-            return self.unidades(self.x,False)
-        elif len(self.x)==2:
-            if int(self.x) in list(range(10,30))+list(range(30,101,10)):
-                return self.decenas(self.x)
-            else:
-                return '{} y {}'.format(self.decenas(self.x[0]+'0'),self.unidades(self.x[1]))
+        else:
+            return self.unidades(x)
 
-print(Numesp(91))
+    def centenas(self, x, ciento=False):
+        if x == '100':
+            if ciento == True:
+                return 'Ciento'
+            else:
+                return 'Cien'
+        elif x == '200':
+            return 'Doscientos'
+        elif x == '300':
+            return 'Trescientos'
+        elif x == '400':
+            return 'Cuatrocientos'
+        elif x == '500':
+            return 'Quinientos'
+        elif x == '600':
+            return 'Seiscientos'
+        elif x == '700':
+            return 'Setecientos'
+        if x == '800':
+            return 'Ochocientos'
+        if x == '900':
+            return 'Novecientos'
+        else:
+            return self.decenas(x)
+
+    def tres(self, x, trae=False):
+        if len(str(x)) == 1:
+            return self.unidades(x, False)
+        elif len(x) == 2:
+            if int(x) in list(range(10, 30)) + list(range(30, 101, 10)):
+                return self.decenas(x)
+            else:
+                return '{} y {}'.format(self.decenas(x[0] + '0'), self.unidades(x[1]))
+        elif len(str(x)) == 3:
+            if int(x) in list(range(100, 1001, 100)):
+                return self.centenas(x)
+            else:
+                return '{} {} y {}'.format(self.centenas(x[0] + '00', ciento=True), self.decenas(x[1] + '0'),
+                                           self.unidades(x[2]))
+        else:
+            return 'error'
+
+    def seis(self, x, trae=False):
+        if x[-3:]=='000':
+            if x[0]=='1':
+                return 'Mil'
+            else:
+                return self.tres(x[0])+' Mil'
+        elif (1000 <= int(x) < 2000):
+            return 'Mil ' + self.tres(x[1:])
+        elif (3 < len(x) <= 6):
+            return '{} Mil {}'.format(x[0:len(x) - 3],self.tres(x[-3:]))
+        else:
+            return 'error'
+
+
+print('', Numesp(1001))
