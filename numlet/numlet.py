88d31cdf65c53b0a527743f3eb0952351c1e1a7c
roylanmartinez <gilbertroylan.martinez@e-campus.uab.cat>
2020-01-30 21:04:20 +0100

    'continuacion1'

--- numlet/numlet.py
+++ numlet/numlet.py
@@ -1,166 +1,226 @@
-def lrg1(x, uno=True, unn=True):
-    if x == '1':
-        return ' Uno' if uno else ' Un'
-    elif x == '2':
-        return ' Dos'
-    elif x == '3':
-        return ' Tres'
-    elif x == '4':
-        return ' Cuatro'
-    elif x == '5':
-        return ' Cinco'
-    elif x == '6':
-        return ' Seis'
-    elif x == '7':
-        return ' Siete'
-    elif x == '8':
-        return ' Ocho'
-    else:
-        return ' Nueve'
+class Numlet:
+    regla = {
+        1: 'self.millon(self.x)',
+        2: 'self.billon(self.x)'
+    }
 
-
-def lrg2(x, inti=True, uno=True, unn=True):
-    if x[1] == '0':
-        if x == '10':
-            return ' Diez'
-        elif x == '20':
-            return ' Veinte'
-        elif x == '30':
-            return ' Treinta'
-        elif x == '40':
-            return ' Cuarenta'
-        elif x == '50':
-            return ' Cincuenta'
-        elif x == '60':
-            return ' Sesenta'
-        elif x == '70':
-            return ' Setenta'
-        elif x == '80':
-            return ' Ochenta'
+    def __init__(self, x):
+        self.r='self.pnot(100)'
+        self.x = str(x)
+        self.largo = len(str(x))
+        self.resto = x % 6
+        self.tt = 'self.millon(self.x)'
+    def __str__(self):
+        if self.largo < 4:
+            return self.base(self.x)
+        elif self.largo < 7:
+            return self.mil((5*'0'+self.x)[-6:])
         else:
-            return ' Noventa'
-    elif x[0] == '1':
-        if x == '11':
-            return ' Once'
-        elif x == '12':
-            return ' Doce'
-        elif x == '13':
-            return ' Trece'
-        elif x == '14':
-            return ' Catorce'
-        elif x == '15':
-            return ' Quince'
-        elif x == '16':
-            return ' Dieciséis'
-        elif x == '17':
-            return ' Diecisiete'
-        elif x == '18':
-            return ' Dieciocho'
-        elif x == '19':
-            return ' Diecinueve'
-    elif x[0] == '2':
-        if x == '21':
-            return ' Veintiuno' if inti else ' Veintiún'
-        elif x == '22':
-            return ' Veintidós'
-        elif x == '23':
-            return ' Veintitrés'
-        elif x == '24':
-            return ' Veinticuatro'
-        elif x == '25':
-            return ' Veinticinco'
-        elif x == '26':
-            return ' Veinteséis'
-        elif x == '27':
-            return ' Veintisiete'
-        elif x == '28':
-            return ' Veintiocho'
-        else:
-            return ' Veintinueve'
-    else:
-        if x[0] == '3':
-            return ' Treinta y' + lrg1(x[1])
-        elif x[0] == '4':
-            return ' Cuarenta y' + lrg1(x[1])
-        elif x[0] == '5':
-            return ' Cincuenta ' + lrg1(x[1])
-        elif x[0] == '6':
-            return ' Sesenta y' + lrg1(x[1])
-        elif x[0] == '7':
-            return ' Setenta y' + lrg1(x[1])
-        elif x[0] == '8':
-            return ' Ochenta y' + lrg1(x[1])
-        else:
-            return ' Noventa y' + lrg1(x[1])
+            pass
 
-
-def lrg3(x, inti=True, uno=True, ciento=True, unn=True):
-    if x == '1':
-        return ' Ciento' if ciento else ' Cien'
-    elif x == '2':
-        return ' Doscientos'
-    elif x == '3':
-        return ' Trescientos'
-    elif x == '4':
-        return ' Cuatrocientos'
-    elif x == '5':
-        return ' Quinientos'
-    elif x == '6':
-        return ' Seiscientos'
-    elif x == '7':
-        return ' Setecientos'
-    elif x == '8':
-        return ' Ochocientos'
-    else:
-        return ' Novecientos'
-
-
-def lrg6(xx):
-    def lrg(x, inti=True, uno=True, ciento=True, unn=True):
-        if x[0] != '0':
-            # verde
-            if x[1] != '0':
-                # verde verde
-                if x[2] != '0':
-                    # verde verde verde
-                    return lrg3(x[0], uno, ciento) + lrg2(x[1:3], inti, uno)
-                else:
-                    # verde verde roja
-                    return lrg3(x[0], uno, ciento) + lrg2(x[1:3])
+    def unidades(self, x, un=False, uno=True):
+        if x == '0' or x == '00' or x == '000':
+            return 'Cero'
+        elif x == '1' or x == '01' or x == '001':
+            if uno:
+                return 'Uno'
             else:
-                # verde roja
-                if x[2] != '0':
-                    # verde roja verde
-                    return lrg3(x[0], uno, ciento) + lrg1(x[2], uno)
-                else:
-                    # verde roja roja
-                    return lrg3(x[0], ciento)
+                return 'Un'
+        elif x == '2' or x == '02' or x == '002':
+            return 'Dos'
+        elif x == '3' or x == '03' or x == '003':
+            return 'Tres'
+        elif x == '4' or x == '04' or x == '004':
+            return 'Cuatro'
+        elif x == '5' or x == '05' or x == '005':
+            return 'Cinco'
+        elif x == '6' or x == '06' or x == '006':
+            return 'Seis'
+        elif x == '7' or x == '07' or x == '007':
+            return 'Siete'
+        elif x == '8' or x == '08' or x == '008':
+            return 'Ocho'
+        elif x == '9' or x == '09' or x == '009':
+            return 'Nueve'
         else:
-            # roja
-            if x[1] != '0':
-                # roja verde
-                if x[2] != '0':
-                    # roja verde verde
-                    return lrg2(x[1:3], inti, uno)
-                else:
-                    # roja verde roja
-                    return lrg2(x[1:3])
+            return 'error'
+
+    def decenas(self, x, un=False):
+        if x == '10' or x == '010':
+            return 'Diez'
+        elif x == '11' or x == '011':
+            return 'Once'
+        elif x == '12' or x == '012':
+            return 'Doce'
+        elif x == '13' or x == '013':
+            return 'Trece'
+        elif x == '14' or x == '014':
+            return 'Catorce'
+        elif x == '15' or x == '015':
+            return 'Quince'
+        elif x == '16' or x == '016':
+            return 'Dieciséis'
+        elif x == '17' or x == '017':
+            return 'Diecisiete'
+        elif x == '18' or x == '018':
+            return 'Dieciocho'
+        elif x == '19' or x == '019':
+            return 'Diecinueve'
+        elif x == '20' or x == '020':
+            return 'Veinte'
+        elif x == '21' or x == '021':
+            if un:
+                return 'Veintiún'
             else:
-                # roja roja
-                if x[2] != '0':
-                    # roja roja verde
-                    return lrg1(x[2], uno)
+                return 'Veintiuno'
+        elif x == '22' or x == '022':
+            return 'Veintidós'
+        elif x == '23' or x == '023':
+            return 'Veintitrés'
+        elif x == '24' or x == '024':
+            return 'Veinticuatro'
+        elif x == '25' or x == '025':
+            return 'Veinticinco'
+        elif x == '26' or x == '026':
+            return 'Veinteséis'
+        elif x == '27' or x == '027':
+            return 'Veintisiete'
+        elif x == '28' or x == '028':
+            return 'Veintiocho'
+        elif x == '29' or x == '029':
+            return 'Veintinueve'
+        elif x == '30' or x == '030':
+            return 'Treinta'
+        elif x == '40' or x == '040':
+            return 'Cuarenta'
+        elif x == '50' or x == '050':
+            return 'Cincuenta'
+        elif x == '60' or x == '060':
+            return 'Sesenta'
+        elif x == '70' or x == '070':
+            return 'Setenta'
+        elif x == '80' or x == '080':
+            return 'Ochenta'
+        elif x == '90' or x == '090':
+            return 'Noventa'
+        else:
+            return self.unidades(x)
+
+    def centenas(self, x, ciento=False):
+        if x == '100':
+            if ciento:
+                return 'Ciento'
+            else:
+                return 'Cien'
+        elif x == '200':
+            return 'Doscientos'
+        elif x == '300':
+            return 'Trescientos'
+        elif x == '400':
+            return 'Cuatrocientos'
+        elif x == '500':
+            return 'Quinientos'
+        elif x == '600':
+            return 'Seiscientos'
+        elif x == '700':
+            return 'Setecientos'
+        if x == '800':
+            return 'Ochocientos'
+        if x == '900':
+            return 'Novecientos'
+        else:
+            return self.decenas(x[1:])
+
+    def base(self, x, un=False, uno=True):
+        if len(x) == 1:
+            return self.unidades(x)
+        elif len(x) == 2:
+            if x[-1] == '0':
+                return self.decenas(x)
+            elif x[0] == '0':
+                return self.unidades(x[0])
+            elif int(x) in list(range(10, 30))+list(range(30, 100, 10)):
+                if un:
+                    return self.decenas(x, un=True)
                 else:
-                    # roja roja roja
-                    return ''
+                    return self.decenas(x)
+            else:
+                return '{} y {}'.format(self.decenas(x[0]+'0'), self.unidades(x[-1]))
+        elif len(x) == 3:
+            if x[0:2] == '00':
+                return self.unidades(x)
+            if x[0] == '0':
+                if int(x[1:]) in list(range(10, 30))+list(range(30, 100, 10)):
+                    if un:
+                        return self.decenas(x, un=True)
+                    else:
+                        return self.decenas(x)
+                else:
+                    if un:
+                        return '{} y {}'.format(self.decenas(x[1]+'0', un=True), self.unidades(x[-1]))
+                    else:
+                        return '{} y {}'.format(self.decenas(x[1]+'0'), self.unidades(x[-1]))
+            if x[1:] == '00':
+                return self.centenas(x)
+            elif x[0] != 0 and x[1] == '0' and x[-1] != '0':
+                if uno:
+                    return '{} {}'.format(self.centenas(x[0]+'00', ciento=True),
+                                          self.unidades(x[-1]))
+                else:
+                    return '{} {}'.format(self.centenas(x[0] + '00', ciento=True),
+                                          self.unidades(x[-1], uno=False))
+            elif int(x[1:]) in list(range(10, 30))+list(range(30, 100, 10)):
+                if un:
+                    return '{} {}'.format(self.centenas(x[0] + '00', ciento=True),
+                                          self.decenas(x[1:], un=True))
+                else:
+                    return '{} {}'.format(self.centenas(x[0]+'00', ciento=True),
+                                          self.decenas(x[1:]))
+            else:
+                return '{} {} y {}'.format(self.centenas(x[0]+'00', ciento=True),
+                                           self.decenas(x[1]+'0'),
+                                           self.unidades(x[-1]))
+        else:
+            return 'error'
 
-    return lrg(xx[0:3], uno=False, ciento=False) + ' Mil' + lrg(xx[3:6], inti=False, ciento=False)
+    def mil(self, x):
+        y = 'Mil'
+        if len(str(int(x))) == 4:
+            if x[3:6] == '000':
+                if x[2] == 1:
+                    return y
+                else:
+                    return f'{self.base(x[2])} {y}'
+            else:
+                if x[2] == '1':
+                    return f'{y} {self.base(x[3:6])}'
+                else:
+                    return '{} {} {}'.format(self.base(x[2]), y, self.base(x[3:6]))
+        elif len(str(int(x))) == 5:
+            if x[3:6] == '000':
+                return f'{self.base(x[:3])} {y}'
+            else:
+                return '{} {} {}'.format(self.base(x[1:3]), y, self.base(x[3:6]))
+        else:
+            if x[1:3] == '01':
+                if x[3:6] == '000':
+                    return f'{self.base(x[:3], uno=False)} {y}'
+                else:
+                    return '{} {} {}'.format(self.base(x[:3], uno=False), y, self.base(x[3:6]))
+            else:
+                if x[3:6] == '000':
+                    return '{} {}'.format(self.base(x[:3], un=True), y)
+                else:
+                    return '{} {} {}'.format(self.base(x[:3], un=True), y, self.base(x[3:6], un=True))
 
-# for i in range(1, 1000000):
-#     print(i, largo_6(str('00000' + str(i))[-6:]))
-# print('Novecientos Noventa y Ocho')
-# print(lrg6('101101'))
-for i in range(100000, 100100):
-    print(lrg6(str(i)))
-# d81b3c6f5be4568572aa8db540b33dfb9f9f0e74
-# asdas123123
+    def millon(self, x):
+        y= 'Millones'
+        if self.resto == 0:
+            return '{} {}'.format(self.mil(x[:3]), y)
+        else:
+            'error'
+
+# for i in range(900000, 1000000):
+#     print(i, Numlet(i))
+print(Numlet(101101))
+'kjkjhkjh'
