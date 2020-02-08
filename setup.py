78942b74ab1e53bc8a63c692554597ff4342b070
roylanmartinez <gilbertroylan.martinez@e-campus.uab.cat>
2020-02-08 15:55:22 +0100

    setup.py cambio

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Paquete-Numlet-roylanmartinez", # Replace with your own username
+    name="Paquete-Numlet-roylanmartinez",
     version="0.0.1",
     author="Roylan Martinez Vargas",
     author_email="roylanmartinez97@gmail.com",
